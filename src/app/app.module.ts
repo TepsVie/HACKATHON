3222f7a7187290dd351131cfed29973ffed06623
TepsVie <teps.vie@yahoo.com>
2018-06-26 13:08:11 -1000

    Home Page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,12 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
+//Page
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
+//Plugins
+import { Push } from '@ionic-native/push';
 @NgModule({
   declarations: [
     MyApp,
@@ -14,7 +17,7 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -24,6 +27,7 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
+    Push,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
